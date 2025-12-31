4d175a686119f43a67766b81d54cfe72ef301b45
Lamaalzahr <Lamatalzahrani@outlook.com>
2025-12-31 13:56:34 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('#contact form');
 
 form.addEventListener('submit', async (e) => {
-e.preventDefault(); 
+e.preventDefault();
 
 const data = {
 name: form.name.value,
@@ -27,27 +27,64 @@ alert('حدث خطأ في الاتصال بالخادم.');
 }
 });
 
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu-items');
+const overlay = document.getElementById('overlay');
+
 menuBtn.addEventListener('click', () => {
-document.body.classList.toggle('body-blur');
+menu.classList.toggle('show');       
+overlay.classList.toggle('hidden');  
+document.body.classList.toggle('body-blur'); 
+});
+
+overlay.addEventListener('click', () => {
+menu.classList.remove('show');
+overlay.classList.add('hidden');
+document.body.classList.remove('body-blur');
+});
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(link => {
+link.addEventListener('click', e => {
+e.preventDefault(); 
+
+const targetId = link.getAttribute('href').substring(1);
+const targetSection = document.getElementById(targetId);
+
+if (targetSection) {
+window.scrollTo({
+top: targetSection.offsetTop - 100, 
+behavior: 'smooth'
+});
+}
+
+if (!menu.classList.contains('translate-x-full')) {
+menu.classList.add('translate-x-full');
+overlay.classList.add('hidden');
+document.body.classList.remove('body-blur');
+}
+});
 });
 
 const sections = document.querySelectorAll("section[id]");
-const navLinks = document.querySelectorAll(".nav-link");
 
 function setActiveLink() {
 let current = sections[0].id;
 
 sections.forEach(section => {
 if (window.scrollY >= section.offsetTop - 150) {
-    current = section.id;
+current = section.id;
 }
 });
 
 navLinks.forEach(link => {
 link.classList.remove("active");
 if (link.getAttribute("href") === "#" + current) {
-    link.classList.add("active");
+link.classList.add("active");
 }
 });
 }
+
+window.addEventListener('scroll', setActiveLink);
 
